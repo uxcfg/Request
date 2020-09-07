feaf546ce7438062cba3515f14884faf16b83df6
Uxcfg <saint69rus@gmail.com>
2020-09-07 21:32:15 +0300

    //

--- src/js/components/callback.js
+++ src/js/components/callback.js
@@ -1,8 +1,14 @@
-const tel = document.querySelector('#callback-btn');
+const tel = document.querySelector('#callback-btn-desktop');
+const telMob = document.querySelector('#callback-btn');
 const form = document.querySelector('#callback-form');
 const img = document.querySelector('#callback-btn img');
 const link = document.querySelector('#callback-btn a');
 const cart = document.querySelector('#cart-mobile img');
+
+telMob.addEventListener('click', function () {
+  this.classList.toggle('active');
+  form.classList.toggle('active');
+});
 
 tel.addEventListener('click', function () {
   this.classList.toggle('active');
